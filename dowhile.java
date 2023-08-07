af2434ceccbca4f7523797482266debf134d99d4
somyagupta0204 <141519317+somyagupta0204@users.noreply.github.com>
2023-08-07 23:25:57 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,21 @@
+
+public class dowhile {
+    public static void main(String args[]){
+        
+        int i=10,n=1;
+        
+       
+        do{
+            System.out.println(i);
+            i--;
+        }
+        while(i>=n);
+
+
+
+
+    }
+
+
+    
+}
