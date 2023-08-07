b049e8cfcd64ca26f028efc989dbf5b4bf4c7796
somyagupta0204 <141519317+somyagupta0204@users.noreply.github.com>
2023-08-07 23:27:17 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class evenodd{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        int num;
+        System.out.println("enter the number");
+        num=input.nextInt();
+        if(num%2==0){
+        
+            System.out.println("it is even");
+        }
+        
+        else{
+            System.out.println("it is odd");
+        }
+        
+
+    }
+
+}
+    
+    
+
