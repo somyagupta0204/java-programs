682dbca87aab0aaefc3d16781c59f1a14e5eb2a3
somyagupta0204 <141519317+somyagupta0204@users.noreply.github.com>
2023-08-07 23:28:16 +0530

    Add files via upload

---
+++ jaigla.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class jaigla {
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("jai gla");
+        
+        
+
+    }
+    
+}
