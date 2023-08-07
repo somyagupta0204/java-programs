265775dc929dbf2ff5733c8bf9d3e3bae01fbdf7
somyagupta0204 <141519317+somyagupta0204@users.noreply.github.com>
2023-08-07 23:31:15 +0530

    Add files via upload

---
+++ check.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class check {
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        int n;
+        System.out.println("enter the value of n");
+        n=input.nextInt();
+        if(n%3==0 && n%5==0){
+            System.out.println("it is divisible by 3 and 5");
+        }
+            else{
+                System.out.println("it is not divisible by 3 and 5");
+                
+            }
+        }
+    }
+    
+
