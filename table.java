7c3f72fdcc76cec7d0c0a3a3f86fbd49784b049d
somyagupta0204 <141519317+somyagupta0204@users.noreply.github.com>
2023-08-07 23:29:50 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class table {
+    public static void main(String args[]){
+        Scanner input=new Scanner (System.in);
+        int n;
+        System.out.println("enter the value of n");
+        n=input.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(n+"*"+i+"="+n*i);
+        }
+
+
+    }
+    
+}
