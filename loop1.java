e4ced511ac210084de0b4bf8aa1baf5c2046c59c
somyagupta0204 <141519317+somyagupta0204@users.noreply.github.com>
2023-09-15 01:20:12 +0530

    Add files via upload

---
+++ loop1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class loop1 {
+    public static void main(String args[]){
+        Scanner input=new Scanner (System.in);
+        System.out.println("enter the value of n");
+        int n=input.nextInt();
+        int i=1;
+        while(i<=n){
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
